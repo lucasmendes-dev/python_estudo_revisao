a7559af4795acb359912c2bc9148b1b13c2b602e
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-11-14 20:47:59 -0300

    POO

--- POO/aplicando_poo.py
+++ POO/aplicando_poo.py
@@ -11,40 +11,46 @@ class ContaCorrente:
         return horario_BR.strftime('%d/%m/%Y %H:%M:%S') #formatação de horário
 
     def __init__(self, nome, cpf, agencia, num_conta):
-        self.nome = nome
-        self.cpf = cpf
-        self.saldo = 0
-        self.limite = None
-        self.agencia = agencia
-        self.num_conta = num_conta
-        self.transacoes = []
+        self._nome = nome
+        self._cpf = cpf
+        self._saldo = 0
+        self._limite = None
+        self._agencia = agencia
+        self._num_conta = num_conta
+        self._transacoes = []
 
     def consultar_saldo(self):
-        print(f"Seu saldo atual é de R${self.saldo:.2f}")
+        print(f"Seu saldo atual é de R${self._saldo:.2f}")
 
     def depositar(self, valor):
-        self.saldo += valor
-        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
+        self._saldo += valor
+        self._transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))
         
     def _limite_conta(self):     #o uso da "_" antes do nome é uma convenção para a função ser privada
-        self.limite = -1000
-        return self.limite
+        self._limite = -1000
+        return self._limite
 
     def sacar_dinheiro(self, valor):
-        if self.saldo - valor < self._limite_conta():
+        if self._saldo - valor < self._limite_conta():
             print("Você não tem saldo suficiente para sacar esse valor.")
             self.consultar_saldo()
         else:
-            self.saldo -= valor
-            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
+            self._saldo -= valor
+            self._transacoes.append((-valor, self._saldo, ContaCorrente._data_hora()))
 
     def consultar_limite_chequeEspecial(self):
         print(f"Seu limite de Cheque Especial é de R${self._limite_conta():.2f}")
         
     def consultar_historico_transacoes(self):
         print("Histórico de Transações:")
-        for transacao in self.transacoes:
+        for transacao in self._transacoes:
             print(transacao)
+            
+    def transferir(self, valor, conta_destino):
+        self._saldo -= valor
+        self._transacoes.append((-valor, self._saldo, ContaCorrente._data_hora()))
+        conta_destino.saldo += valor
+        conta_destino._transacoes.append((-valor, conta_destino.saldo, ContaCorrente._data_hora()))
         
 
 conta = ContaCorrente("Lucas", "02080277693", 3610, 33794)
@@ -55,10 +61,16 @@ conta.consultar_saldo()
 conta.depositar(600)
 conta.consultar_saldo()
 
-conta.sacar_dinheiro(2100)
 
 print("Saldo final")
 conta.consultar_limite_chequeEspecial()
 
 print("-" * 20)
 conta.consultar_historico_transacoes()
+
+print("-" * 20)
+conta_2 = ContaCorrente("Maria", "12345678900", 1234, 66584)
+conta.transferir(240, conta_2)
+
+conta.consultar_saldo()
+conta_2.consultar_saldo()
